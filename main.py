e2e2a16c7bae7ab133454b6ac55a157801db476a
T4Tom <58210134+T4Tom@users.noreply.github.com>
2020-01-26 10:35:08 +0000

    Update main.py

--- main.py
+++ main.py
@@ -11,6 +11,10 @@ shipimg1 = pygame.image.load('Ship1.png')
 shipimg1 = pygame.transform.scale(shipimg1, (100, 160))
 shipimg2 = pygame.image.load('Ship2.png')
 shipimg2 = pygame.transform.scale(shipimg2, (100, 160))
+mineimg1 = pygame.image.load('mine1.png')
+mineimg1 = pygame.transform.scale(mineimg1, (50, 50))
+mineimg2 = pygame.image.load('mine2.png')
+mineimg2 = pygame.transform.scale(mineimg2, (50, 50))
 
 
 
@@ -22,6 +26,14 @@ def game_loop():
   shiplocation1 = [450, 400]
   shiplocation2 = [250, 400]
   
+  mine1y = random.randint(0, 300)
+  mine1x = random.randint(0, 750)
+  mine2y = random.randint(0, 300)
+  mine2x = random.randint(0, 750)
+  
+  minelocation1 = [mine1x, mine1y]
+  minelocation2 = [mine2x, mine2y]
+  
   done = False
   while not done:
     
@@ -32,7 +44,8 @@ def game_loop():
     pointstextRect.center = (400, 50)
     
     
-    
+    screen.blit(mineimg1, minelocation1)
+    screen.blit(mineimg2, minelocation2)
     screen.blit(shipimg1, shiplocation1)
     screen.blit(shipimg2, shiplocation2)
     
