b6e87503ea6b564a650730d03b116cf916c4a728
T4Tom <58210134+T4Tom@users.noreply.github.com>
2020-02-07 14:10:28 +0000

    Update main.py

--- main.py
+++ main.py
@@ -30,7 +30,8 @@ explosionimg = pygame.transform.scale(explosionimg, (100, 100))
 
 def game_over():
   screen.blit(deathtext, deathtextRect)
-  
+  screen.blit(shipimg1, shiplocation1)
+  screen.blit(shipimg2, shiplocation2)     
   pygame.display.flip()
   time.sleep(2)
   game_loop()
@@ -106,8 +107,7 @@ def game_loop():
 
     screen.blit(mineimg1, minelocation1)
     screen.blit(mineimg2, minelocation2)
-    screen.blit(shipimg1, shiplocation1)
-    screen.blit(shipimg2, shiplocation2)    
+ 
     
     if hit1:
       shot1 = False
@@ -160,7 +160,8 @@ def game_loop():
     if pressed[pygame.K_a] and shiplocation2[0] >= 0:
       shiplocation2[0] -= 3
 
-      
+    screen.blit(shipimg1, shiplocation1)
+    screen.blit(shipimg2, shiplocation2)         
       
       
 
