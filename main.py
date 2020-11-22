5177d0e83ac41aa988e41c41e97143be29eacb89
T4Tom <58210134+T4Tom@users.noreply.github.com>
2020-11-22 10:41:13 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import pygame, time, random
+import pygame, time, random, sys
 pygame.init()
 screen = pygame.display.set_mode((800, 600))
 clock = pygame.time.Clock()
@@ -32,20 +32,15 @@ explosionimg = pygame.image.load('explosion.png')
 explosionimg = pygame.transform.scale(explosionimg, (100, 100))
 
 
-start_ticks = pygame.time.get_ticks() # 'import' the timer
+COUNTDOWN = pygame.USEREVENT
+TIMER = pygame.USEREVENT + 1
 
-
-def game_over():
-    seconds = 0
-    screen.fill((0, 0, 0))
-    screen.blit(deathtext, deathtextRect)
-    pygame.display.flip()
-    time.sleep(2)
-    game_loop()
     
 
 
 def game_loop():
+
+    seconds = 5
         
     level = 1
     gainedpoints = 100
@@ -71,22 +66,13 @@ def game_loop():
     
 
     
+    pygame.time.set_timer(COUNTDOWN, 5000)
+    pygame.time.set_timer(TIMER, 1000)
     
-    
-    done = False
-    while not done:
+    game_active = False
+    while True:
         
-        screen.fill((0, 0, 0))
-        
-
-
-        seconds = (pygame.time.get_ticks()-start_ticks)/1000 # initiates the counter
-        if seconds > 5: # if the seconds count up to 10
-            second = 0
-            game_over()
-            game_loop()
-        print(seconds)
-        
+        screen.fill((0, 0, 0))    
         
 
         minelocation1 = [mine1x, mine1y]
@@ -110,14 +96,21 @@ def game_loop():
         
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                exit()
+                pygame.quit()
+                sys.exit()
             if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
-                exit()
+                pygame.quit()
+                sys.exit()
             if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
                 shot1 = True
                 shot2 = True
             if event.type == pygame.KEYDOWN and event.key == pygame.K_RETURN:
                 game_loop()
+            if event.type == COUNTDOWN:
+                game_active = False
+                game_loop()
+            if event.type == TIMER:
+                seconds -= 1
                 
                 
             
@@ -126,21 +119,76 @@ def game_loop():
         if laser2_rect.colliderect(mine2_rect):
             hit2 = True
             
-
-        screen.blit(mineimg1, minelocation1)
-        screen.blit(mineimg2, minelocation2)
- 
+            
+        secondstext = normalfont.render("Seconds: {0}".format(seconds), True, (255, 255, 255))
+        secondstextRect = secondstext.get_rect(center = (200, 200))
+         
+            
+        if not shot1:
+            laserx = shiplocation1[0] + 48
+            lasery = 480
+                
+        elif shot1:
+            lasery -= 10
+            pygame.draw.rect(screen, (150, 0, 0), pygame.Rect(laserx, lasery, 5, 40))
+            if lasery < -100:
+                game_active = False
         
-        if hit1:
+        if not shot2:
+            laserx2 = shiplocation2[0] + 48
+            lasery2 = 480
+
+        
+        elif shot2:
+            lasery2 -= 10
+            pygame.draw.rect(screen, (178, 102, 255), pygame.Rect(laserx2, lasery2, 5, 40))
+            if lasery2 < -100:
+                game_active = False
+            
+                
+                
+                
+                
+                
+        pressed = pygame.key.get_pressed()
+        if pressed[pygame.K_RIGHT] and shiplocation1[0] + 100 <= 800:
+            shiplocation1[0] += 6
+        if pressed[pygame.K_LEFT] and shiplocation1[0] >= 0:
+            shiplocation1[0] -= 6
+            
+        if pressed[pygame.K_d] and shiplocation2[0] + 100 <= 800:
+            shiplocation2[0] += 6
+        if pressed[pygame.K_a] and shiplocation2[0] >= 0:
+            shiplocation2[0] -= 6
+                 
+        
+        pointstext = normalfont.render('Points: {0}'.format(score), True, (255, 255, 255))
+        pointstextRect = pointstext.get_rect()
+        pointstextRect.center = (600, 50)
+        
+        leveltext = normalfont.render('Level: {0}'.format(level), True, (255, 255, 255))
+        leveltextRect = leveltext.get_rect()
+        leveltextRect.center = (200, 50)
+        
+        if game_active:
+            screen.blit(mineimg1, minelocation1)
+            screen.blit(mineimg2, minelocation2)
+            screen.blit(shipimg1, shiplocation1)
+            screen.blit(shipimg2, shiplocation2)  
+            screen.blit(pointstext, pointstextRect)        
+            screen.blit(leveltext, leveltextRect)
+            screen.blit(secondstext, secondstextRect)
+
+        if hit1 and game_active:
             shot1 = False
             screen.blit(explosionimg, explosionimgRect)
         
-        if hit2:
+        if hit2 and game_active:
             shot2 = False
             screen.blit(explosionimg, explosionimgRect2)
             
             
-        if hit1 and hit2:
+        if hit1 and hit2 and game_active:
             screen.blit(explosionimg, explosionimgRect)
             screen.blit(explosionimg, explosionimgRect2)
             screen.blit(shipimg1, shiplocation1)
@@ -158,62 +206,15 @@ def game_loop():
             level += 1
             score += gainedpoints
             gainedpoints += 100
-            seconds = 0
-            
-            
-            
-        if not shot1:
-            laserx = shiplocation1[0] + 48
-            lasery = 480
-                
-        elif shot1:
-            lasery -= 10
-            pygame.draw.rect(screen, (150, 0, 0), pygame.Rect(laserx, lasery, 5, 40))
-            if lasery < -100:
-                game_over()
-        
-        if not shot2:
-            laserx2 = shiplocation2[0] + 48
-            lasery2 = 480
+            pygame.time.set_timer(COUNTDOWN, 5000)
+            pygame.time.set_timer(TIMER, 1000)
+            seconds = 5  
 
-        
-        elif shot2:
-            lasery2 -= 10
-            pygame.draw.rect(screen, (178, 102, 255), pygame.Rect(laserx2, lasery2, 5, 40))
-            if lasery2 < -100:
-                game_over()
-            
-                
-                
-                
-                
-                
-        pressed = pygame.key.get_pressed()
-        if pressed[pygame.K_RIGHT] and shiplocation1[0] + 100 <= 800:
-            shiplocation1[0] += 6
-        if pressed[pygame.K_LEFT] and shiplocation1[0] >= 0:
-            shiplocation1[0] -= 6
-            
-        if pressed[pygame.K_d] and shiplocation2[0] + 100 <= 800:
-            shiplocation2[0] += 6
-        if pressed[pygame.K_a] and shiplocation2[0] >= 0:
-            shiplocation2[0] -= 6
+        if not game_active:
+            screen.fill((0, 0, 0))
+            if pressed[pygame.K_UP]:
+                game_active = True
 
-        screen.blit(shipimg1, shiplocation1)
-        screen.blit(shipimg2, shiplocation2)            
-        
-        
-        pointstext = normalfont.render('Points: {0}'.format(score), True, (255, 255, 255))
-        pointstextRect = pointstext.get_rect()
-        pointstextRect.center = (600, 50)
-        screen.blit(pointstext, pointstextRect)        
-        
-        leveltext = normalfont.render('Level: {0}'.format(level), True, (255, 255, 255))
-        leveltextRect = leveltext.get_rect()
-        leveltextRect.center = (200, 50)
-        screen.blit(leveltext, leveltextRect)
-            
-            
 
         pygame.display.flip()
         clock.tick(70)
@@ -223,6 +224,4 @@ game_loop()
 
 # Ship 1 = red, right
 # Ship 2 = purple, left
-
-
 
